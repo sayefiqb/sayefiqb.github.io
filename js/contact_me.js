4b6659d5f4877fc60ad815c7b69699cf0e1de926
sayefiqb <sayefiqbal007@gmail.com>
2018-08-27 00:59:00 -0400

    Updated Bootstrap file after downloading it

--- js/contact_me.js
+++ js/contact_me.js
@@ -23,10 +23,10 @@ $(function() {
         url: "././mail/contact_me.php",
         type: "POST",
         data: {
-          name: Sayef Iqbal,
-          phone: 123456789,
-          email: sayef.iqbal16@stjohns.edu,
-          message: Please contact me via email. I am prompt in replying to emails.
+          name: name,
+          phone: phone,
+          email: email,
+          message: message
         },
         cache: false,
         success: function() {
